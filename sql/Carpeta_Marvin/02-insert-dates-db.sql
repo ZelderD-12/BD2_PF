15abba8c1ad5b61be0333fc7615e77fbe5ec055d
Gustavo Adolfo Tobías Ramírez <tobiasgusito@outlook.com>
2026-04-21 22:25:56 -0600

    Cambios para agregar la vistas y LAs carpetas del sql

---
+++ sql/Carpeta_Marvin/02-insert-dates-db.sql
@@ -0,0 +1,6 @@
+---Datos de la Tabla de Roles---
+INSERT INTO roles (rol) VALUES ('Paciente');
+INSERT INTO roles (rol) VALUES ('Resepcionista');
+INSERT INTO roles (rol) VALUES ('Trabajad@r');
+INSERT INTO roles (rol) VALUES ('Doct@r');
+
--- sql/init/01-create-clinica-db.sql
+++ sql/init/01-create-clinica-db.sql
@@ -7,3 +7,11 @@ GO
 
 USE ClinicaDB;
 GO
+
+CREATE TABLE ClinicaDB.dbo.[roles] (
+    Idrol tinyint IDENTITY(1,1) NOT NULL,
+    rol varchar(100) NOT NULL,
+    CONSTRAINT role_pk PRIMARY KEY (Idrol)
+);
+GO
+
